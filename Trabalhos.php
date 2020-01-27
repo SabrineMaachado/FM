2d136c181967f7ca36440f8d02ec57131095d1c3
Sabrine <sa.machado4@gmail.com>
2020-01-27 15:40:32 -0300

    update sobre

--- Trabalhos.php
+++ Trabalhos.php
@@ -63,16 +63,5 @@
 <script src="https://kit.fontawesome.com/0d30721436.js" crossorigin="anonymous"></script> 
 
 <script>
-    $(function() {
-    var selectedClass = "";
-    $(".filter").click(function(){
-    selectedClass = $(this).attr("data-rel");
-    $("#gallery").fadeTo(100, 0.1);
-    $("#gallery div").not("."+selectedClass).fadeOut().removeClass('animation');
-    setTimeout(function() {
-    $("."+selectedClass).fadeIn().addClass('animation');
-    $("#gallery").fadeTo(300, 1);
-    }, 300);
-    });
-    });
+
     </script>
